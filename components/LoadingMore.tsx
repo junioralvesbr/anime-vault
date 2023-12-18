d564b46c1be7e3d7257b6408e167ed77064ce468
Junior Alves <jrnalves@gmail.com>
2023-12-18 16:03:27 -0300

    change ref for div

--- components/LoadingMore.tsx
+++ components/LoadingMore.tsx
@@ -10,27 +10,32 @@ let page = 2
 
 export function LoadingMore() {
   const [data, setData] = useState<AnimeProp[]>([])
-  const [ref, inView] = useInView()
+
+  const [ref, inView] = useInView({
+    threshold: 0
+  })
 
   useEffect(() => {
     if (inView) {
+
       fetchAnime(page).then(res => {
         setData([...data, ...res])
         page++
       })
+
     }
-    console.log(inView)
-  }, [inView, data])
+
+  }, [inView])
 
   return (
-    <div ref={ref}>
+    <div>
       <div className="grid grid-cols-4 gap-14 mt-10">
         {data.map(anime => (
           <AnimeCard anime={anime} />
         ))}
       </div>
 
-      <div className="flex justify-center mt-10">
+      <div ref={ref} className="flex justify-center mt-10">
         <Image
           src="/spinner.svg"
           alt="Icone de spinner"
@@ -38,7 +43,6 @@ export function LoadingMore() {
           height={80}
         />
       </div>
-      <h2>{`teste de view ${inView}`}</h2>
     </div>
   )
 }
